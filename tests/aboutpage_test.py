6b933787e6ad8ed947dceb070d64681993c8b5c4
Divya Gabani <drg5@njit.edu>
2022-03-07 23:56:27 -0500

    Updated testfile

--- tests/aboutpage_test.py
+++
@@ -1,8 +0,0 @@
-"""This test the aboutpage"""
-
-
-def test_request_index(client):
-    """This makes the about page"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"Aboutpage" in response.data
