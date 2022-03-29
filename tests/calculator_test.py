e4908aa48113bdc0423bd64a2e37a643e1a29d32
Divya Gabani <drg5@njit.edu>
2022-03-28 23:03:14 -0400

    calculator

---
+++ tests/calculator_test.py
@@ -0,0 +1,29 @@
+"""Testing the Calculator"""
+# From specifies the namespace
+from calculator import Calculator
+
+def test_calculator_is_instance():
+    calculator = Calculator()
+    assert isinstance(calculator,Calculator)
+
+def test_calculator_add_method():
+    """Testing the Calculator"""
+    # this is show using the calculator object add method
+    tuple_list = (1,2)
+    assert Calculator.add(tuple_list) == 3
+
+def test_calculator_subtract_method():
+    """Testing the Calculator Subtract"""
+    tuple_list = (1,2)
+    assert Calculator.subtract(tuple_list) == -3
+
+
+def test_calculator_multiply_method():
+    """Testing the Calculator Subtract"""
+    tuple_list = (1,2)
+    assert Calculator.multiply(tuple_list) == 2
+
+def test_calculator_divide_method():
+    """Testing the Calculator Subtract"""
+    tuple_list = (1,2)
+    assert Calculator.divison(tuple_list) == 2
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -8,6 +8,10 @@ def test_main_menu_links(client):
     assert b'<a class="nav-link" aria-current="page" href="/python" style="color: white;">Python</a>' in response.data
     assert b'<a class="nav-link" aria-current="page" href="/html1" style="color: white;">CI/CD</a>' in response.data
     assert b'<a class="nav-link" aria-current="page" href="/git" style="color: white;">Git</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/oop" style="color: white;">OOPS</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/pylint" style="color: white;">Pylint</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/solid" style="color: white;">SOLID</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/aaa" style="color: white;">AAA</a>' in response.data
 
 def test_request_example(client):
     """This makes the index page"""
@@ -42,3 +46,27 @@ def test_python_page(client):
     response = client.get("/python")
     assert response.status_code == 200
     assert b"Let's take a closer look to Python" in response.data
+
+def test_oop_page(client):
+    """This makes the docker page"""
+    response = client.get("/oop")
+    assert response.status_code == 200
+    assert b"OOP Page" in response.data
+
+def test_solid_page(client):
+    """This makes the docker page"""
+    response = client.get("/solid")
+    assert response.status_code == 200
+    assert b"Solid Page" in response.data
+
+def test_aaa_page(client):
+    """This makes the docker page"""
+    response = client.get("/aaa")
+    assert response.status_code == 200
+    assert b"AAA Page" in response.data
+
+def test_pylint_page(client):
+    """This makes the docker page"""
+    response = client.get("/pylint")
+    assert response.status_code == 200
+    assert b"Pylint Page" in response.data
